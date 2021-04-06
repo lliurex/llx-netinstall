eac6d5269c3fa73a1835f928cad4f56a8015881c
M.Angel Juan <m.angel.juan@gmail.com>
2021-04-06 14:03:45 +0200

    Fix netinstall menu

--- install.llx-netinstall/usr/share/llxbootmanager/www-boot/pxemenu.d/90-netinstall.php
+++ install.llx-netinstall/usr/share/llxbootmanager/www-boot/pxemenu.d/90-netinstall.php
@@ -1,14 +1,32 @@
 <?php
 
 /* Check Netinstall  */
-
-$mirror_var="/var/lib/n4d/variables-dir/LLIUREXMIRROR";
-if (is_file($mirror_var) ) $mirror_installed=True; else $mirror_installed=False;
-$filename="/etc/ltsp/bootopts/netinstall.json";
+$distribution = 'llx21';
+$distribution_name ='focal';
+$mirror_var="/var/lib/n4d/variables/LLIUREXMIRROR";
+$filename="/etc/llxnetinstall/netinstall.json";
 $type_install='';
 
-$json=array();
+if (is_file($mirror_var)){
+    try{
+        $fp = file_get_contents($mirror_var);
+        $content = json_decode($fp,true);
+        if (array_key_exists('LLIUREXMIRROR',$content) && array_key_exists('value',$content['LLIUREXMIRROR']) && array_key_exists($distribution,$content['LLIUREXMIRROR']['value'])){
+            $data_mirror = $content['LLIUREXMIRROR']['value'][$distribution];
+            if (array_key_exists('status_mirror',$data_mirror) && array_key_exists('progress',$data_mirror) && strtolower($data_mirror['status_mirror']) == 'ok' && $data_mirror['progress'] == 100 ){
+                $mirror_installed = true;
+            }else{
+                $mirror_installed = false;
+            }
+        }else{
+            $mirror_installed = false;
+        }
+    }catch(Exception $e){
+        $mirror_installed = false;
+    }
+}
 
+$json=array();
 if (is_file($filename)){
     $content_json=file_get_contents($filename);
     $json=json_decode($content_json,true);
@@ -22,13 +40,13 @@ if (is_file($filename)){
 }
 
 
-if (is_array($json) && array_key_exists("netinstall_boot",$json) && strtolower($json["netinstall_boot"])=="true"&&$mirror_installed==True){
+if (is_array($json) && array_key_exists("netinstall_boot",$json) && strtolower($json["netinstall_boot"]) == "true" && $mirror_installed==True){
    $MenuEntryList=array();
    $MenuEntry=new stdClass();
    $MenuEntry->id="netinstall";
    $MenuEntry->label="Instal·la LliureX en aquest ordinador";
    $MenuEntry->menuString="";
-   $dirname = '/var/www/mirror/llx19/dists/bionic/main/';
+   $dirname = "/var/www/mirror/$distribution/dists/$distribution_name/main/";
    $dir=false;
    if (is_dir($dirname)){
         $dir=scandir($dirname);
